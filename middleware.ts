8f42c06bc44023d969755467b6aa2bb297dc2f28
Dillon Erhardt <dillon.erhardt@gmail.com>
2025-01-14 14:50:52 +0000

    (feature/map): revert middleware changes

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,12 @@
-import { NextResponse, NextRequest } from "next/server";
+import { type NextRequest } from "next/server";
 import { updateSession } from "./lib/supabase/middleware";
 
 export async function middleware(request: NextRequest) {
-  const res = NextResponse.next();
-
-  // Update Supabase session
   return await updateSession(request);
 }
 
 export const config = {
   matcher: [
-    "/((?!api/|_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
   ],
 };
